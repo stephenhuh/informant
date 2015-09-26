7f453ff94251a90e70f67f5b5bb5f18b0176efaf
Stephen Huh <stephenhuh@gmail.com>
2015-09-26 19:34:30 -0400

    add post

--- server/index.js
+++ server/index.js
@@ -16,8 +16,9 @@ app.get('/', function(req, res) {
   // capture youtube screen
   exec('screencapture -R106,157,640,360 ./public/screenshot.jpeg', function (error, stdout, stderr){
   });
+  detectFace();
   source = fs.createReadStream('./public/screenshot.jpeg');
-  source.pipe(request.post('http://localhost:1337/image'));
+  source.pipe(request.post('http://localhost:1337/image')); //oxford req
   res.render('index.ejs');
 });
 
@@ -25,6 +26,28 @@ app.post('/image', function(req, res) {
   destination = fs.createWriteStream("./written.jpeg");
   req.pipe(destination);
 });
+
+var detectFace = function(){
+	request({
+		url: "https://api.projectoxford.ai/face/v0/detections"
+		qs: {
+			analyzesFaceLandmarks : "true",
+			analyzesAge : "true",
+			analyzesGender : "true",
+			analyzesHeadPose: "true"
+		}
+		method: "POST",
+		headers: {
+			"Content-Type" : "application/json"
+			"Ocp-Apim-Subscription-Key"	: "0067f293c7b6446db4bde094604c4426",
+		}
+		body: {
+			"url":"http://www.nndb.com/people/397/000022331/conan-obrien-1-sized.jpg" 
+		}
+	}, function(response){
+		console.log(response);
+	})
+}
 
 server.listen(1337, function() {
   console.log('Server is running on port 1337');
